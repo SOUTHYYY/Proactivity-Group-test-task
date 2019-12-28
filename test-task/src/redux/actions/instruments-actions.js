21ea610b76150e3c3c0031265ff243cf85a797e7
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-28 18:37:13 +0300

    setTotalCompaniesCount get payload from getCompanies request

--- test-task/src/redux/actions/instruments-actions.js
+++ test-task/src/redux/actions/instruments-actions.js
@@ -11,33 +11,40 @@ import {
 } from '../consts/instrumentConsts'
 
 
-const setIsLoading = (payload) => ({ type: SET_IS_LOADING, payload})
+const setIsLoading = (payload) => ({ type: SET_IS_LOADING, payload })
+const getIsSortedByRate = () => ({ type: GET_IS_SORTED_BY_RATE })
+const getIsSortedByWorksCount = () => ({ type: GET_IS_SORTED_BY_WORKS_COUNT })
+const getIsSortedByPartnersCount = () => ({ type: GET_IS_SORTED_BY_PARTNERS_COUNT })
+const setCurrentSort = (sort) => (dispatch) => {
+    if (sort === "works_count") {
+        dispatch(getIsSortedByWorksCount())
+    } else if (sort === "partners_count") {
+        dispatch(getIsSortedByPartnersCount())
+    } else if (sort === "rate") {
+        dispatch(getIsSortedByRate())
+    }
+}
 export const setCurrentPage = (payload) => ({ type: SET_CURRENT_PAGE, payload })
 export const isFavoriteActionCreator = (companyId) => ({ type: IS_FAVORITE, companyId })
-export const getIsSortedByRate = () => ({ type: GET_IS_SORTED_BY_RATE })
-export const getIsSortedByWorksCount = () => ({ type: GET_IS_SORTED_BY_WORKS_COUNT })
 export const setTotalCompaniesCount = (totalCount) => ({ type: SET_TOTAL_COMPANIES_COUNT, totalCount })
-export const getIsSortedByPartnersCount = () => ({ type: GET_IS_SORTED_BY_PARTNERS_COUNT })
 export const setCompanies = (data) => ({ type: SET_COMPANIES, data })
 export const requestCompanies = (instrumentTypeCode, currentPage) => async (dispatch) => {
-    debugger
-    console.log(currentPage)
     dispatch(setIsLoading(true))
     await CompaniesAPI.getCompanies(instrumentTypeCode, currentPage)
         .then(data => {
-            dispatch(setCompanies(data))
+            console.log(data)
+            dispatch(setCompanies(data.data))
+            dispatch(setTotalCompaniesCount(data.totalCount))
             dispatch(setIsLoading(false))
         })
 }
-export const getTotalCompaniesCount = () => async (dispatch) => {
-    let totalCompaniesCount = await CompaniesAPI.getTotalCount()
-    dispatch(setTotalCompaniesCount(totalCompaniesCount))
-}
-export const sotrCompanies = (sort) => async dispatch => {
-        dispatch(setIsLoading(true))
-        await CompaniesAPI.getCompaniesBySort(sort)
+
+export const sortCompanies = (sort) => async dispatch => {
+    dispatch(setIsLoading(true))
+    dispatch(setCurrentSort(sort))
+    await CompaniesAPI.getCompaniesBySort(sort)
         .then(data => {
             dispatch(setCompanies(data))
             dispatch(setIsLoading(false))
         })
-    }
+}
