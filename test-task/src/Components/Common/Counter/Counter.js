159e2f778df419a1333ed8f47ec3ffc9574dc6eb
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-28 14:58:12 +0300

    Now anginator it's func Component

--- test-task/src/Components/Common/Counter/Counter.js
+++ test-task/src/Components/Common/Counter/Counter.js
@@ -1,12 +1,27 @@
-const counter = (totalCompaniesCount) => {
+import React from 'react';
+import styles from './Panginator.module.css'
+
+
+const Panginator = ({ totalCompaniesCount, handlePageChanged }) => {
     let pagesCount = Math.ceil(totalCompaniesCount / 20);
     let pages = [];
-
-    for (let i = 1; i <= pagesCount; i++) {
+    // должно быть больше чем <= pagesCount
+    for (let i = 1; i <= 10; i++) {
         pages.push(i);
     }
 
-    return pages
+    return (
+        <nav className={styles.panginator}>
+            <ul>
+                {pages.map(item => (
+                    <li key={item}
+                        onClick={() => handlePageChanged(item)}>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
 
-export default counter
+export default Panginator
