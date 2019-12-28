3c5416837af31cb2805229a45cca4d55a70ddc12
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-28 18:39:40 +0300

    add setCurrentSort func that dispatch a sort type for request

--- test-task/src/Components/Instruments/Instruments.jsx
+++ test-task/src/Components/Instruments/Instruments.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import styles from "./Instruments.module.css";
 import CompanyItem from "./CompanyItem/CompanyItem";
 import sort from "../../image/sort-down.svg";
-import counter from "../Common/Counter/Counter";
+import Panginator from "../Common/Counter/Counter";
+import { NavLink } from "react-router-dom";
 
 const Instruments = ({
   companies,
@@ -10,15 +11,10 @@ const Instruments = ({
   isFavoriteActionCreator,
   isSortedByRate,
   totalCompaniesCount,
-  sotrCompanies,
-  getIsSortedByRate,
-  requestCompanies,
+  sortCompanies,
   isSortedByWorksCount,
-  getIsSortedByWorksCount,
   isSortedByPartnersCount,
-  getIsSortedByPartnersCount,
-  setCurrentPage,
-  onPageChanged
+  setCurrentPage
 }) => {
   let companyItems = companies.map(item => {
     const { id } = item;
@@ -31,51 +27,14 @@ const Instruments = ({
       />
     );
   });
+  console.log(isSortedByRate)
 
-  const sotrCompanysByRate = () => {
-    if (isSortedByRate === false) {
-      sotrCompanies("rate");
-      getIsSortedByRate();
-    } else {
-      requestCompanies();
-      getIsSortedByRate();
-    }
-  };
-  const sotrCompanysByRateByWorkCount = () => {
-    if (isSortedByWorksCount === false) {
-      sotrCompanies("works_count");
-      getIsSortedByWorksCount();
-    } else {
-      requestCompanies();
-      getIsSortedByWorksCount();
-    }
-  };
-  const sortCompanysByPartnersCount = () => {
-    if (isSortedByPartnersCount === false) {
-      sotrCompanies("partners_count");
-      getIsSortedByPartnersCount();
-    } else {
-      requestCompanies();
-      getIsSortedByPartnersCount();
-    }
-  };
+  const setCurrentSotr = sotrType => {
+    sortCompanies(sotrType)
+  }
   const handlePageChanged = p => {
     setCurrentPage(p);
-    onPageChanged();
   };
-  const panginator = counter(totalCompaniesCount).map(p => {
-    return (
-      <span
-        key={p}
-        className={styles.currentPage && styles.selectedPage}
-        onClick={() => {
-          handlePageChanged(p);
-        }}
-      >
-        {p}
-      </span>
-    );
-  });
   const projectsImage = isSortedByWorksCount ? (
     <img src={sort} alt="sorted"></img>
   ) : null;
@@ -83,40 +42,45 @@ const Instruments = ({
     <img src={sort} alt="sorted"></img>
   ) : null;
   const rateImage = isSortedByRate ? <img src={sort} alt="sorted"></img> : null;
-
   return (
     <div className={styles.Instruments}>
-      <div className={styles.Instruments_header}>
-        <div className={styles.Instruments_header__titles}>Название</div>
-        <div
-          className={styles.Instruments_header__projects}
-          onClick={sotrCompanysByRateByWorkCount}
-        >
-          Проекты
-          {projectsImage}
-        </div>
-        <div
-          className={styles.Instruments_header__partners}
-          onClick={sortCompanysByPartnersCount}
-        >
-          Партнеры
-          {partnersImage}
-        </div>
-        <div
-          onClick={sotrCompanysByRate}
-          className={styles.Instruments_header__rate}
-        >
-          Оценка пользователей
-          {rateImage}
-        </div>
-        <div>Сравнить</div>
-      </div>
-      <div className={styles.Instruments_items}>
-        <table>
-          <tbody>{companyItems}</tbody>
-        </table>
-      </div>
-      <div className={styles.pages}>{panginator}</div>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <div>Название</div>
+            </th>
+            <th>
+              <NavLink  to='/' onClick={() => setCurrentSotr('works_count')}>
+                Проекты
+                {projectsImage}
+              </NavLink>
+            </th>
+            <th>
+              <div onClick={() => setCurrentSotr('partners_count')}>
+                Партнеры
+                {partnersImage}
+              </div>
+            </th>
+            <th>
+              <div
+                onClick={() => setCurrentSotr('rate')}
+              >
+                Рейтинг
+                {rateImage}
+              </div>
+            </th>
+            <th>
+              <div>Сравнить</div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>{companyItems}</tbody>
+      </table>
+      <Panginator
+        totalCompaniesCount={totalCompaniesCount}
+        handlePageChanged={handlePageChanged}
+      />
     </div>
   );
 };
