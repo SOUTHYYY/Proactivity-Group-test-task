03e511c6ee87b261f9ad4baae10d5147e608a8fc
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-28 15:42:36 +0300

    Pulled totalCount from a shared date object

--- test-task/src/API/api.js
+++ test-task/src/API/api.js
@@ -13,7 +13,10 @@ export const CompaniesAPI = {
     getCompanies(instrumentTypeCode = 'cms', currentPage = '1') {
         return instance.get(`instrumentsList?instrument_type_code=${instrumentTypeCode}&page=${currentPage}`)
             .then(response => {
-                return transformData(response.data.data)
+                return {
+                    data: transformData(response.data.data),
+                    totalCount: response.data.total,
+                }
             })
     },
     getCompaniesBySort(sort = 'partners_count', instrumentTypeCode = 'cms', sortDirection = 'desc', currentPage = '1'){
@@ -22,10 +25,4 @@ export const CompaniesAPI = {
                 return transformData(response.data.data)
             })
     },
-    getTotalCount(sort = 'partners_count', instrumentTypeCode = 'cms', sortDirection = 'desc', currentPage = '1'){
-        return instance.get(`instrumentsList?instrument_type_code=${instrumentTypeCode}&sort_direction=${sortDirection}&sort=${sort}&page=${currentPage}`)
-            .then(response => {
-                return (response.data.total)
-            })
-    }
 }
